7599dbb2f1f5fb46ed481cbf66920fb385e2454c
Willian Freitas <willian.freitasoliveira@yahoo.com.br>
2021-07-01 21:57:57 -0300

    Projeto inicial com tomcat 9

---
+++ src/main/java/br/com/will/gerenciador/servlet/OlaMundoServlet.java
@@ -0,0 +1,25 @@
+package br.com.will.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OlaMundoServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Will Karioca</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
