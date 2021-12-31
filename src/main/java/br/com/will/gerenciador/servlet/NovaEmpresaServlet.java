cbd914b3a86cea2794257a3798accd7c990e8938
Willian Freitas <willian.freitasoliveira@yahoo.com.br>
2021-12-31 17:37:01 -0300

    Entendendo Get e Post

--- src/main/java/br/com/will/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/will/gerenciador/servlet/NovaEmpresaServlet.java
@@ -16,9 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
-		String nomeEmpresa = request.getParameter("nome");
+		
+		String nomeEmpresa = request.getParameter("nomeEmpresa");
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!!</body></html>");
 	}
