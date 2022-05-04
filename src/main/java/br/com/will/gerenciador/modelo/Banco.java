83966abcbf2b11c99e70c1dbde0a4f125d304fd7
Willian Freitas <willian.freitasoliveira@yahoo.com.br>
2022-05-04 09:37:44 -0300

    Criando formulário de Login

--- src/main/java/br/com/will/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/will/gerenciador/modelo/Banco.java
@@ -7,19 +7,29 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> empresas = new ArrayList<>();
+	private static List<Usuario> usuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setId(chaveSequencial++);
 		empresa.setNome("Empresa1");
-		
 		Empresa empresa2 = new Empresa();
 		empresa2.setNome("Empresa2");
 		empresa2.setId(chaveSequencial++);
 		
 		empresas.add(empresa);
 		empresas.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("will");
+		u1.setSenha("1234");
+		Usuario u2 = new Usuario();
+		u2.setLogin("amanda");
+		u2.setSenha("12345");
+		
+		usuarios.add(u1);
+		usuarios.add(u2);
 	}
 	
 	public void adiciona(Empresa empresa) {
@@ -49,6 +59,15 @@ public class Banco {
 				return empresa;
 			}
 			
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : usuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
 		}
 		return null;
 	}
